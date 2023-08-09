ddecbc571a59e7a3b4418fd08e6d9fe597f9bbc8
marista032 <marista032@gmail.com>
2023-08-09 05:54:19 -0700

    task 1 - install Aphrodite: Modify the Login component and Delete the file Login.css

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.test.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import Login from './Login';
-import { shallow } from  'enzyme';
+import { StyleSheetTestUtils } from 'aphrodite';
 
+describe('<Login />', () => {
+  beforeEach (() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
 
-describe('<Login>', () => {
-    test('renders without crashing', () => {
-        shallow(<Header />);
-    });
+  afterEach (() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+  it('renders without crashing', () => {
+    shallow(<Login />);
+  });
 
-    test('rendera 2 input tags and 2 label tags', () => {
-        const wrapper = shallow(<Login />);
-        expect(wrapper.find('input')).toHaveLength(2);
-        expect(wrapper.find('label')).toHaveLength(2);
-    });
+  it('renders 2 input tags and 2 label tags', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find('input').length).toBe(2); // Verifying that there are two input tags
+    expect(wrapper.find('label').length).toBe(2); // Verifying that there are two label tags
+  });
 });
