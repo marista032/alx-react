6cf213cce5ef8d86b1416b46d9f5a075899d0f13
marista032 <marista032@gmail.com>
2023-08-09 05:47:49 -0700

    task 1 - install Aphrodite: Modify the Header component and Delete the file Header.css

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,14 +1,29 @@
-import './Header.css';
-import logo from '../assets/holberton-logo.jpg';
 import React from 'react';
+import logo from '../assets/holberton-logo.jpg';
+import { StyleSheet, css} from 'aphrodite';
 
-
-export default function Hader () {
+function Header() {
+  const styles = StyleSheet.create({
+    AppHeader: {
+      backgroundColor: '#ffffff',
+      display: 'flex',
+      alignItems: 'center',
+      fontSize: '20px',
+      color: '#e0354b',
+      borderBottom: '3px solid #e0354b',
+      textAlign: 'center',
+      fontFamily: 'Arial, sans-serif',
+  },
+  AppLogo: {
+    width: '200px',
+  }
+  });
     return (
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
-        
-      </header>
+        <div className={css(styles.AppHeader)}>
+          <img src={logo} className={css(styles.AppLogo)} alt="logo" />
+          <h1>School dashboard</h1>
+        </div>
     )
-}
+};
+
+export default Header;
--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Header from './Header';
+import { StyleSheetTestUtils } from 'aphrodite';
 
+describe('<Header />', () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
 
-describe('<Header />', () =>{
-    test('Header component renders without crashing', () => {
-       shallow(<Header />);
-    });
+  afterEach(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
 
+  it('renders without crashing', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
 
-    test('renders img and h1 tags', () => {
-        const wrapper = shallow(<Header />);
-        expect(wrapper.find('img')).toHaveLength(1);
-        expect(wrapper.find('h1')).toHaveLength(1);
-    });
+  it('renders img and h1 tags', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('img').length).toBe(1); // Verifying that there is one img tag
+    expect(wrapper.find('h1').length).toBe(1); // Verifying that there is one h1 tag
+  });
 });
