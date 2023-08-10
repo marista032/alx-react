56d63a58ca252a15d6f056937dbf96201ddd55b7
marista032 <marista032@gmail.com>
2023-08-10 15:16:35 -0700

    task 2: Conditionally applying style - Modify the CourseListRow component

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseList.js
@@ -1,37 +1,65 @@
 import React from 'react';
-import CourseListRow from './CourseListRow';
-import './CourseList.css';
 import PropTypes from 'prop-types';
 import CourseShape from './CourseShape';
+import CourseListRow from './CourseListRow';
+import { StyleSheet, css } from 'aphrodite';
 
-
-export default function CourseList ({ listCourses }) {
-    if (listCourses.length === 0) {
-        return <div>No courses available yet</div>
+const CourseList = ({ listCourses }) => {
+  const styles = StyleSheet.create({
+    table: {
+      marginTop: '2em',
+      width: '100%',
+      border: '1px solid #ddd',
+      fontSize: '1.2rem',
+      marginBottom: '15em',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+    th: {
+      borderBottom: '1px solid #ddd',
+    },
+    td: {
+      width: '80%', 
+    },
+    th: {
+      width: '80%', 
+    },   
+    tr: {
+      textAlign: 'left',
+      border: '1px solid #ddd',
     }
-    return (
-        <table id="CourseList">
-            <thead>
-                <CourseListRow textFirstCell="Available courses" isHeader={true} />
-                <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
-            </thead>
-            <tbody>
-                {listCourses.length > 0 ? (
-                    listCourses.map(({ id, name, credit}) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
-                ) : (
-                    <CourseListRow textFirstCell='No course available yet' />
-                )}
 
-            </tbody>
-           
-        </table>
-    );
-}
+  });
+  return (
+    <table id="CourseList" className={css(styles.table)}>
+      <thead >
+        <CourseListRow isHeader={true} textFirstCell="Available courses" />
+        <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
+      </thead>
+      <tbody className={css(styles.body)}>
+        {listCourses.length === 0 ? (
+          <CourseListRow isHeader={false} textFirstCell="No course available yet" />
+        ) : (
+          listCourses.map((course) => (
+            <CourseListRow
+              key={course.id}
+              isHeader={false}
+              textFirstCell={course.name}
+              textSecondCell={course.credit.toString()}
+            />
+          ))
+        )}
+      </tbody>
+    </table>
+  );
+};
 
-CourseList.PropTypes = {
-    listCourses: PropTypes.arrayOf(CourseShape),
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
 };
 
 CourseList.defaultProps = {
-    listCourses: [],
+  listCourses: [],
 };
+
+export default CourseList;
--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseShape.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseShape.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
-export default function CourseShape() {
-  return PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    credit: PropTypes.number.isRequired,
-  });
-}
+const CourseShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  credit: PropTypes.number.isRequired,
+});
+
+export default CourseShape;
