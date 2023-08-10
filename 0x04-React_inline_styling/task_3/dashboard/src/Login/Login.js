55b2512e1c8a869b5382469cbb46922be75633ca
marista032 <marista032@gmail.com>
2023-08-10 15:35:58 -0700

    task 3: Responsive design - Modify the component Login

--- 0x04-React_inline_styling/task_3/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/Login/Login.js
@@ -1,15 +1,44 @@
-import './Login.css';
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
-export default function Login () {
+function Login() {
+    const styles = StyleSheet.create({
+        App: {
+            textAlign: 'center',
+            fontFamily: 'Arial, sans-serif',
+            '@media (max-width: 900px)': {
+                display: 'block',
+            },
+        },          
+        AppBody: {
+            margin: '10px',
+            textAlign: 'left',
+        },
+        inputGroup: {
+            marginBottom: '10px',
+        },
+        input: {
+            marginRight: '10px',
+        }
+    });
     return (
-        <body className="App-body">
-            <p>Login to access the full dashboard</p>
-            <label htmlFor="email">Email:</label>
-            <input type="email" name="email" id="email"></input>
-            <label htmlFor="password">Password:</label>
-            <input type="password" name="password" id="password"></input>
-            <button>OK</button>
-      </body>
-    );
+        <div className={css(styles.App)}>
+            <div className={css(styles.AppBody)}>
+                <p>Login to access the full dashboard</p>
+
+                <div className={css(styles.inputGroup)}>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" id="email" className={css(styles.input)} />
+                </div>
+
+                <div className={css(styles.inputGroup)}>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" id="password" className={css(styles.input)} />
+                </div>
+                <button>OK</button>
+            </div>  
+        </div>
+    )
 }
+
+export default Login;
