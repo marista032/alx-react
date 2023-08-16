f02d78291079e0c4bd221d8ebd6609022bd02080
marista032 <marista032@gmail.com>
2023-08-16 09:49:04 -0700

    task 0 - Converting into an Immutable object using fromJS

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,10 @@
+const { fromJS, Map } = require('immutable');
+
+function getImmutableObject(object) {
+  const immutableMap = fromJS(object);
+  return immutableMap;
+}
+
+module.exports = {
+  getImmutableObject
+};
